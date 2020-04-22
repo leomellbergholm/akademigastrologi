75e50a9137bd2b6c28fb1c05dc54845db873d93a
leomellbergholm <62606313+leomellbergholm@users.noreply.github.com>
2020-04-22 15:53:56 +0200

    databas o system

--- aghemsida/python/system.py
+++ aghemsida/python/system.py
@@ -1,7 +1,11 @@
+# coding: utf-8
+# Author: Leo M.H & Axel H
 #Modulen som kopplar PostgresSQL till databasen pg.server.mah.se
 import psycopg2
 #Modul som hjälper till att skapa ett slumpmässigt ID
 import random
+#
+from bottle import route, run, template, request, static_file, redirect
 
 #Huvudfunktionen
 def main():
@@ -13,3 +17,45 @@ def main():
             user = "ah8140",
             password = "pzvieemm")
 
+    cur = con.cursor()
+
+@route("/")
+def index(cur, con):
+    '''
+    Startsida
+    '''
+    
+    if request.query.info != "":
+        info = request.query.info
+
+    return template("index", articles=list_articles(), info=info)
+
+@route('/akademigastrologi/<pagename>/')
+def show_article(pagename, info = ""):
+    """Displays a single article (loaded from a text file)."""
+    
+    file = open("akademigastrologi/" + pagename + ".txt", "r", encoding ="utf-8")
+    text = file.read()
+    
+    if request.query.info != "":
+        info = request.query.info
+
+    return template("article", article=pagename, text=text, info=info)
+
+@route('/akademigastrologi/<pagename>')
+def delete_article(pagename):
+    ''' 
+    Deletes chosen article and redirects
+    '''
+    try:
+        file = remove("wiki/" + pagename + ".txt")
+
+        redirect("/?info=" + pagename + " borttagen")
+
+    except FileNotFoundError:
+        redirect ("/=info=" + pagename + " kan inte hittas")
+
+
+    
+    cur.execute("insert into traveler (persID, name, email, phone) values({}, '{}', '{}', {})".format(id_in, name_in, email_in, phone_in))
+    con.commit() #Måste commit() för att reflektera den inmatade datan
--- aghemsida/sql/database.sql
+++ aghemsida/sql/database.sql
@@ -1,3 +1,5 @@
+psql -U ah8140 -d ah8140 -h pgserver.mah.se
+
 \c ah8140
 drop database if exists akademigastrologi;
 create database akademigastrologi
@@ -9,3 +11,34 @@ drop table if EXISTS ;
 drop table if EXISTS ;
 
 \c akademigastrologi;
+
+create table users(
+ userID serial,
+ recipeID serial,
+ firstname varchar(20),
+ lastname varchar(30),
+ username varchar(20),
+ email varchar(30),
+ password varchar(20),
+ primary key(userID, recipeID),
+ );
+
+create table recipes(
+ recipeID int,
+ userID int,
+ title varchar(30),
+ ingress varchar(250),
+ instructions text,
+ primary key (recipeID, userID),
+ foreign key (recipeID) references users (recipeID),
+ foreign key (userID) references users (userID)
+ ); 
+
+ create table ingredients(
+ recipeID int,
+ ingridient_name varchar(30),
+ quantity int,
+ measurement varchar(10)
+ primary key (recipeID),
+ foreign key (recipeID) references users (recipeID)
+ ); 
